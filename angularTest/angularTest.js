b821ac515fa266d911473facc91af8d7112aa60d
Luke Rossy <lrossy@gmail.com>
2015-01-21 14:15:44 -0500

    test1

---
+++ angularTest/angularTest.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Luke on 21/01/2015.
+ */
+var app = angular.module('demoApp', ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/',
+        {
+            controller: 'SimpleController',
+            templateUrl: 'partials/View1.html'
+        })
+        .when('/view2',
+        {
+            controller: 'SimpleController',
+            templateUrl: 'partials/View2.html'
+        })
+        .otherwise({redirectTo: '/'});
+});
+
+app.controller('SimpleController',
+    function SimpleController($scope) {
+        $scope.customers = [
+            {name: 'John Doe', city: 'New York'},
+            {name: 'Jane Doe', city: 'San Fransisco'},
+            {name: 'Joe Smith', city: 'Washington'}
+        ];
+
+        $scope.addCustomer = function() {
+            $scope.customers.push(
+                {
+                    name: $scope.newCustomer.name,
+                    city: $scope.newCustomer.city
+                }
+            );
+        };
+    }
+);
